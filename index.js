e555be155c9dc6b4c93ac12873cbcd395234ecc9
Chris Nady <nady.christopher@gmail.com>
2017-08-31 03:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ function createRock(x) {
    * Now that we have a rock, we'll need to append
    * it to GAME and move it downwards.
    */
-    GAME.addEventListener(rock)
+    GAME.Add(rock)
   /**
    * This function moves the rock. (2 pixels at a time
    * seems like a good pace.)
